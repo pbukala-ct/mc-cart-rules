d08e55ec83e8824e7ae3e2473975e5c56513b738
pbukala-ct <piotr.bukala@commercetools.com>
2023-08-29 13:56:35 +1000

    entry-point change

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'woolies';
+export const entryPointUriPath = 'cart-validation';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
